e202c34afbfe44448fa2cfc87a57fe98c1bb02d3
Shubham <shubham11729@gmail.com>
2023-04-14 13:34:03 -0500

    margin added

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,27 +1,28 @@
 'use strict';
 
-let email = 'newUser@gmail.com';
-let password = 'newUser1234';
 
 
-
+localStorage.setItem('email', 'newUser@gmail.com');
+localStorage.setItem('password', 'newUser1234');
 
 
 let button = document.querySelector('.btn-1');
 let error = document.querySelector('.error')
 
 function getInfo() {
-    let userName = document.querySelector('#email');
-    let passWord = document.querySelector('#password');
+    let userName = document.querySelector('.email');
+    let passWord = document.querySelector('.password');
+    let correctEmail = localStorage.getItem('email');
+    let correctPassword = localStorage.getItem('password');
 
-
-        if (userName.value === email && passWord.value === password) {
-            window.location.href = '../home.html'
+        if (userName.value === correctEmail && passWord.value === correctPassword) {
+          location.href = '../home.html';
             console.log('Hey there');
         } else error.innerHTML = '*Incorrect username or password*'
 }
 
 
-button.addEventListener ('click', function(){
+button.addEventListener ('click', () =>{
   getInfo();
 });
+
